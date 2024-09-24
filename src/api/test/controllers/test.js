981c4c2c0bcee94c2516c3b870ed5c2d5a64a77b
Nick-tsoukas <nick.tsoukas101@gmail.com>
2024-09-24 15:21:40 -0700

    chore: adding route

--- src/api/test/controllers/test.js
+++ src/api/test/controllers/test.js
@@ -1,9 +1,10 @@
 // ./src/api/test/controllers/test.js
 
 module.exports = {
-    async testConnectivity(ctx) {
-      ctx.send({
-        message: 'Test route is working!',
-      });
-    },
-  };
+  async testMessage(ctx) {
+    // Send a simple "Hello, world!" response
+    ctx.send({
+      message: 'Hello, world!',
+    });
+  },
+};
--- src/api/test/routes/test.js
+++ src/api/test/routes/test.js
@@ -1,13 +1,14 @@
+// ./src/api/test/routes/test.js
+
 module.exports = {
-    routes: [
-      {
-        method: 'GET',
-        path: '/test-connectivity',
-        handler: 'test.testConnectivity',
-        config: {
-          auth: false, // Disable auth to make it easy to access for testing
-        },
+  routes: [
+    {
+      method: 'GET',                        // HTTP method
+      path: '/test-route',                   // Path to access the route
+      handler: 'test.testMessage',           // Points to the controller function
+      config: {
+        auth: false,                         // No authentication needed for this route
       },
-    ],
-  };
-  
+    },
+  ],
+};
